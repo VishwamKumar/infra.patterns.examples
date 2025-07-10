cd9b49d7041d8315cd6e93d024bae6224abd5eaf
Vishwam <vishwamkumar@outlook.com>
2025-07-10 12:32:17 -0400

    updated nuget package

--- src/WeatherApp.RestApi.RedisCache/GlobalUsings.cs
+++ src/WeatherApp.RestApi.RedisCache/GlobalUsings.cs
@@ -1,4 +1,4 @@
-﻿global using Vishwam.RedisLibrary.Caching.Services;
+﻿global using Vishwam.Caching.Redis.Services;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Logging;
 global using WeatherApp.RestApi.RedisCache.Dtos;
