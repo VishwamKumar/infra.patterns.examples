76a60302bfb4fe7b6e5120d7040f958bb6ffa36b
Vishwam <vishwamkumar@outlook.com>
2025-07-15 00:07:31 -0400

    .

--- src/WeatherApp.RestApi.UsingRabbitMQ/GlobalUsings.cs
+++ src/WeatherApp.RestApi.UsingRabbitMQ/GlobalUsings.cs
@@ -1,4 +1,4 @@
-﻿global using Vishwam.Queuing.RabbitMQ.Services;
+﻿global using Vishwam.Queuing.RabbitMQ;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Extensions.Logging;
 global using WeatherApp.RestApi.UsingRabbitMQ.Dtos;
