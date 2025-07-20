130af7a089b4dce2b2bfd8f22335d1d5ccb0a1b3
Vishwam <vishwamkumar@outlook.com>
2025-07-20 18:22:34 -0400

    updated

--- src/WeatherApp.RestApi.UsingRedisCache/Controllers/WeatherForecastController.cs
+++ src/WeatherApp.RestApi.UsingRedisCache/Controllers/WeatherForecastController.cs
@@ -36,13 +36,12 @@ public class WeatherForecastController(IRedisCacheService redisCacheService,
     {
         try
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            })];
         }
         catch (Exception ex)
         {
